6b573005b5fb41ee9abe04910d2ee7ee924833a8
David Harrigan <davidharrigan@users.noreply.github.com>
2022-10-31 21:58:39 -0400

    chore: increase memory

--- infra/vm/k8s-nodes.tf
+++ infra/vm/k8s-nodes.tf
@@ -11,7 +11,7 @@ resource "proxmox_vm_qemu" "k8s-server" {
   cores    = 4
   sockets  = 1
   cpu      = "host"
-  memory   = 4096
+  memory   = 6144
   scsihw   = "virtio-scsi-pci"
   bootdisk = "scsi0"
 
